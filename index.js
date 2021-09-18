dbcf4e4ae163c0d20c1588737cede2eb334d4ca3
Danilo Marques <danilomarques20@hotmail.com>
2021-09-17 23:11:47 -0300

    Refactoring

--- index.js
+++ index.js
@@ -6,47 +6,103 @@ const mongoose = require('mongoose');
 const COMMENT_QUEUE = 'COMMENT_QUEUE'; 
 const STATUS_CHANGE_QUEUE = 'STATUS_CHANGE_QUEUE'; 
 
-async function db() {
-  await mongoose.connect(process.env.MONGO_URL);
-}
-
-async function openConnection() {
+async function execute() {
   try {
+    console.log('Starting...');
+
     await db();
-    const conn = await connect(process.env.RABBIT_URL);
-    const channel = await conn.createChannel();
-    await channel.consume(COMMENT_QUEUE, async function(msgBytes) {
-      if (msgBytes == null) return;
-      const msgString = msgBytes.content.toString();
-      const msgJson = JSON.parse(msgString);
-      const fpost = await PostModel.findOne({_id: msgJson.post_id});
+    const channel = await openConnectionAndChannel();
+    await consume(channel, COMMENT_QUEUE, sendEmailAndSaveMessage);
+    await consume(channel, STATUS_CHANGE_QUEUE, sendEmailStatus);
 
-      if (fpost) return;
-
-      const post = new PostModel({
-        _id: msgJson.post_id,
-        postAuthorName: msgJson.post_author_name,
-        postAuthorEmail: msgJson.post_author_email,
-      });
-      await post.save();
-      const comment = {
-        commentAuthorName: msgJson.comment_author_name,
-        commentAuthorEmail: msgJson.comment_author_email
-      };
-
-      await PostModel.updateOne(
-        {
-        _id: msgJson.post_id
-        },
-        {
-          $addToSet: { comments: comment }
-        }
-      ); 
-
-    }, { noAck: true });
   } catch(e) {
+    // TODO how to recover from error
     console.log(e);
   }
 }
 
-openConnection();
+async function db() {
+  await mongoose.connect(process.env.MONGO_URL);
+}
+
+async function openConnectionAndChannel() {
+  try {
+    const conn = await connect(process.env.RABBIT_URL);
+    const channel = await conn.createChannel();
+    return channel;
+  } catch(e) {
+    throw e;
+  }
+}
+
+async function consume(channel, queueName, callback) {
+  await channel.consume(queueName, callback, { noAck: true });
+}
+
+async function sendEmailAndSaveMessage(msgBytes) {
+  if (msgBytes == null) return;
+  const msgString = msgBytes.content.toString();
+  const msgJson = JSON.parse(msgString);
+
+  console.log({ msgJson });
+
+  const { post_author_email, comment_author_email } = msgJson;
+
+  await sendEmail({ post_author_email, comment_author_email} );
+
+  await saveMessage(msgJson);
+}
+
+async function sendEmailStatus(msgBytes) {
+  // TODO
+  if (msgBytes == null) return;
+  const msgString = msgBytes.content.toString();
+  const msgJson = JSON.parse(msgString);
+
+  console.log({ msgJson });
+
+  const { post_id } = msgJson;
+  const post = await PostModel.findOneAndDelete({_id: post_id});
+  if (!post) return;
+
+  const comments = post.comments;
+  comments.forEach(comment => {
+    console.log(`send email to ${comment.commentAuthorEmail}`);
+  });
+
+}
+
+async function sendEmail({ post_author_email, comment_author_email }) {
+  console.log(`Send email to ${post_author_email}`);
+}
+
+async function saveMessage(msgJson) {
+  let post = await PostModel.findOne({_id: msgJson.post_id});
+
+  if (!post) {
+    post = new PostModel({
+      _id: msgJson.post_id,
+      postAuthorName: msgJson.post_author_name,
+      postAuthorEmail: msgJson.post_author_email,
+    });
+
+    await post.save();
+  }
+
+  const comment = {
+    commentAuthorName: msgJson.comment_author_name,
+    commentAuthorEmail: msgJson.comment_author_email
+  };
+
+  await PostModel.updateOne(
+    {
+      _id: post._id
+    },
+    {
+      $addToSet: { comments: comment }
+    }
+  );
+}
+
+
+execute();
